e541f7415dd4c90028b5327fd3218229d42d64af
alemigo <32462069+alemigo@users.noreply.github.com>
2018-10-25 12:31:48 +0100

    Implemented additional methods +bug fix
    Implemented additional file object methods, and fixed bug in readline

--- splitfile.py
+++ splitfile.py
@@ -1,54 +1,57 @@
 #!/usr/bin/env python3
-#-----------------------------
+# -----------------------------
 # splitfile.py
-#-----------------------------
+# -----------------------------
 #
-"""Python file-like object that facilitates reading and writing of binary data
-   split across multiple volumes (files) of a specified size. Includes support for
-   compression and encryption.
+"""Python file-like object that facilitates reading and writing of
+   binary data split across multiple volumes (files) of a specified
+   size. Includes support for compression and encryption.
 """
 
-version = '1.0.0'
+version = '1.1.0'
 __author__ = 'github.com/alemigo'
 
 # Imports
 from builtins import open as python_open
 import os
 import io
+import lzma
+from Crypto.Cipher import AES
 
 # from splitfile import *
 __all__ = ['SplitFile', 'open']
 
+
 class SplitFile(object):
     """Interface to read and write a stream across multiple volumes"""
-    file = None             # Python file object
-    file_name = None        # Filename of first volume, or iterable of volume filenames to use
-    file_index = 0          # Volume index
-    file_size = 0           # Track size of current volume
-    mode = None             # IO mode: rb, wb
-    volume_size = 0         # Maximum size of each volume
-    compression = False     # Use LZMA compression: True or False
-    lzma_preset = None      # LZMA preset
-    lzma_obj = None         # LZMA Compressor/Decompressor object
-    aes_key = None          # Encryption key for AES, or None for unencrypted operation
-    buffer = None           # Buffer for uncompressed, unencrypted data
-    last_nl = 0             # Position in buffer up to which no \n was found
-    cipher = None           # Cipher object used for AES encryption
-    first_io = True         # First read or write of stream (first volume)
-    EOF = False             # End of all volumes reached for read operation
-    BLOCKSIZE = 20 * 512    # Size of read blocks
+    file = None  # Python file object
+    file_name = None  # Filename of first volume, or iterable of filenames
+    file_index = 0  # Volume index
+    file_size = 0  # Track size of current volume
+    mode = None  # IO mode: rb, wb
+    volume_size = 0  # Maximum size of each volume
+    compression = False  # Use LZMA compression: True or False
+    lzma_preset = None  # LZMA preset
+    lzma_obj = None  # LZMA Compressor/Decompressor object
+    aes_key = None  # Encryption key for AES, or None for unencrypted
+    buffer = None  # Buffer for uncompressed, unencrypted data
+    last_nl = 0  # Position in buffer up to which no \n was found
+    cipher = None  # Cipher object used for AES encryption
+    first_io = True  # First read or write of stream (first volume)
+    EOF = False  # End of all volumes reached for read operation
+    BLOCKSIZE = 20 * 512  # Size of read blocks
 
-    def __init__(self, filename, mode, volume_size=0, compression=False, lzma_preset=6, aes_key=None):
+    def __init__(self, filename, mode, volume_size=0, compression=False,
+                 lzma_preset=6, aes_key=None):
         """Initialize splitfile object"""
         self.file_name = filename
         self.mode = mode
         self.file_index = 0
         self.first_io = True
-
         self.aes_key = aes_key
-        if aes_key: from Crypto.Cipher import AES
 
-        if volume_size < 0: raise ValueError('Volume size must be positive or zero')
+        if volume_size < 0: raise ValueError(
+            'Volume size must be positive or zero')
         self.volume_size = volume_size
 
         if mode not in ['rb', 'wb']:
@@ -56,11 +59,17 @@ class SplitFile(object):
 
         self.compression = compression
         if compression:
-            import lzma
             if mode == 'wb':
                 self.lzma_obj = lzma.LZMACompressor(preset=lzma_preset)
             else:
                 self.lzma_obj = lzma.LZMADecompressor()
+
+        # open first file
+        if not self._next_file():
+            if mode == 'rb':
+                raise FileNotFoundError('File not found')
+            else: #wb
+                raise OSError('Invalid filename')
 
     def __enter__(self):
         return self
@@ -68,11 +77,22 @@ class SplitFile(object):
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.close()
 
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        r = self.readline()
+        if r == b'':
+            raise StopIteration
+        else:
+            return r
+
     def _next_file(self):
-        """Close prior volume if open, and open next volume (if possible in read mode)"""
-        if self.file:
+        """Close prior volume if open, and open next volume (if possible
+           in read mode)
+        """
+        if not self.first_io and self.file:
             self.file.close()
-            self.file = None
             self.file_size = 0
 
         self.file_index += 1
@@ -87,7 +107,8 @@ class SplitFile(object):
         else:
             next_file_name = next(self.file_name)
 
-        # Open the file or return false if in read mode and next file doesn't exist
+        # Open the file or return false if in read mode and next file
+        # doesn't exist
         if self.mode == 'wb' or os.path.exists(next_file_name):
             self.file = python_open(next_file_name, self.mode)
             return True
@@ -96,19 +117,24 @@ class SplitFile(object):
 
     def write(self, data):
         """write method implementation"""
-        if self.mode != 'wb': raise io.UnsupportedOperation('Cannot write in read mode')
+        if self.mode != 'wb': raise io.UnsupportedOperation(
+            'Cannot write in read mode')
 
-        if self.first_io: # first write to first volume
+        if self.first_io:  # first write to first volume
             self.first_io = False
-            self._next_file()
-            if self.aes_key: # write encryption header if needed
+            if self.aes_key:  # write encryption header if needed
                 self.cipher = AES.new(self.aes_key, AES.MODE_CTR)
                 self.file.write(self.cipher.nonce)
 
         # compress and encrypt if needed
         if self.compression: data = self.lzma_obj.compress(data)
         if self.aes_key: data = self.cipher.encrypt(data)
-        self._write(data) # write to file
+        self._write(data)  # write to file
+
+    def writelines(self, lines):
+        """writelines implementation"""
+        for line in lines:
+            self.write(line)
 
     def _write(self, data):
         """write bytes data to file, advance to next volume if needed"""
@@ -117,8 +143,8 @@ class SplitFile(object):
             while len(data) > 0:
                 write_size = 0
                 while write_size == 0:
-
-                    write_size = min(len(data), self.volume_size - self.file_size)
+                    write_size = min(len(data),
+                                     self.volume_size - self.file_size)
                     if write_size > 0:
                         break
                     else:
@@ -130,18 +156,17 @@ class SplitFile(object):
                 data = data[len(output):]
 
             return bytes_written
-        else: # for volume_size of 0, do not split to volumes
+        else:  # for volume_size of 0, do not split to volumes
             return self.file.write(data)
 
-    def read(self, size=-1, line=False):
+    def read(self, size=-1, *, line=False):
         """read method implementation"""
         if self.mode != 'rb':
             raise io.UnsupportedOperation('Cannot read in write mode')
 
-        if self.first_io: # first read from first volume
+        if self.first_io:  # first read from first volume
             self.first_io = False
-            self._next_file()
-            if self.aes_key: # read encryption header
+            if self.aes_key:  # read encryption header
                 nonce = self.file.read(8)
                 self.cipher = AES.new(self.aes_key, AES.MODE_CTR, nonce=nonce)
 
@@ -172,7 +197,7 @@ class SplitFile(object):
                 # try to read from buffer
                 output = self._read_buffer(size, line)
                 if output: return output
-            else: # no more to read, flush buffer
+            else:  # no more to read, flush buffer
                 output = self.buffer
                 self.buffer = b''
                 self.last_nl = 0
@@ -199,7 +224,7 @@ class SplitFile(object):
             if rp == -1:
                 self.last_nl = len(self.buffer)
             else:
-                rp = rp + self.last_nl + 1
+                rp += 1
                 self.last_nl = 0
 
             if size != -1 and len(self.buffer) >= size and size < rp: rp = size
@@ -208,14 +233,14 @@ class SplitFile(object):
 
         if rp == -1:
             return None
-        else: #read from beginning of buffer to position RP
+        else:  # read from beginning of buffer to position RP
             output = self.buffer[:rp]
             self.buffer = self.buffer[rp:]
             return output
 
     def readline(self, size=-1):
         """readline implementation"""
-        return self.read(size, True)
+        return self.read(size, line=True)
 
     def readlines(self, sizehint=0):
         """readlines implementation"""
@@ -233,11 +258,30 @@ class SplitFile(object):
             # flush compression
             data = self.lzma_obj.flush()
             if self.aes_key: data = self.cipher.encrypt(data)
-            self._write(data) # write to file
+            self._write(data)  # write to file
 
         if self.file: self.file.close()
         self.buffer = b''
 
-def open(filename, mode, volume_size=0, compression=False, lzma_preset=6, aes_key=None):
+    @property
+    def closed(self):
+        return self.file.closed
+
+    def readable(self):
+        return self.file.readable()
+
+    def writable(self):
+        return self.file.writable()
+
+    def seekable(self):
+        return False
+
+    def flush(self):
+        self.file.flush()
+
+
+def open(filename, mode, volume_size=0, compression=False, lzma_preset=6,
+         aes_key=None):
     """alternative constructor"""
-    return SplitFile(filename, mode, volume_size, compression, lzma_preset, aes_key)
+    return SplitFile(filename, mode, volume_size, compression, lzma_preset,
+                     aes_key)
